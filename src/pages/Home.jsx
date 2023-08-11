7159c4d5baac39c31489914c001b7a497858ffcc
Rahul Saha <rahulsaha87928@gmail.com>
2023-08-11 20:13:16 +0600

    create loader for loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import Loader from "./Loader";
 
 const Home = () => {
+
+    const { loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <Loader></Loader>
+    }
+
     return (
         <div>
             <h2>This is Home</h2>
