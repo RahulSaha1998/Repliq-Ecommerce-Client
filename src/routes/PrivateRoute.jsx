8164209bccaab9c22d18f254bcf911c28e6fce5e
Rahul Saha <rahulsaha87928@gmail.com>
2023-08-12 00:01:55 +0600

    customized private route

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,20 +1,42 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 import { Navigate } from 'react-router-dom';
 import Loader from '../components/Loader';
+import Swal from 'sweetalert2';
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
-
+    const [showAlert, setShowAlert] = useState(false);
 
     if (loading) {
-        return <Loader></Loader>
+        return <Loader />;
     }
-    
+
     if (user) {
         return children;
     }
-    return <Navigate to='/login'></Navigate>;
+
+    if (showAlert) {
+        return <Navigate to="/login" />;
+    }
+
+    // If showAlert is not true and user is not available, display the alert
+    if (!user && !showAlert) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Access Denied',
+            text: 'You need to be logged in to access this page.',
+            confirmButtonText: 'Login',
+            showCancelButton: true,
+            cancelButtonText: 'Cancel'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                setShowAlert(true);
+            }
+        });
+
+        return null;
+    }
 };
 
 export default PrivateRoute;
