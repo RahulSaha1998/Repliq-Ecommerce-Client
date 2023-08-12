5e5c4a1aa85b41d3616c37ee16d0cd586ac5bab0
Rahul Saha <rahulsaha87928@gmail.com>
2023-08-12 22:35:39 +0600

    Display the cart data in Cart section

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,101 @@
+import { useContext, useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { AuthContext } from '../providers/AuthProvider';
+import { Fade } from 'react-awesome-reveal';
+import Swal from 'sweetalert2';
+import { Link } from 'react-router-dom';
+
 
 const Cart = () => {
+
+    const [cartProduct, setCartProduct] = useState([]);
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/cartProducts/${user?.email}`)
+            .then(res => res.json())
+            .then(data => {
+                setCartProduct(data);
+            })
+    }, [user])
+
+
+    const handleDelete = _id => {
+        console.log(_id);
+        fetch(`http://localhost:5000/cartProducts/${_id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Product Delete Successfully!',
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                    const remaining = cartProduct.filter(product => product._id !== _id);
+                    setCartProduct(remaining);
+                }
+            })
+    }
+
+    //   calculating total price of cart products
+    const total = cartProduct.reduce((sum, item) => item.price + sum, 0);
+
+
     return (
         <div>
             <Helmet>
                 <title>REPLIQ | Cart</title>
             </Helmet>
-            <h2>Hello from cart</h2>
+            <div className="flex items-center gap-5 justify-end mb-5">
+                <p className="text-lg font-semibold">Total: ${total}</p>
+                <Link to="/cart/checkout" state={total}>
+                    <button className='btn btn-outline'>Checkout</button>
+                </Link>
+            </div>
+            <div className="overflow-x-auto">
+                <div className="overflow-x-auto card shadow-lg">
+                    <Fade>
+                        <table className="table table-zebra w-full">
+                            {/* head */}
+                            <thead>
+                                <tr className='bg-slate-400 text-slate-800'>
+                                    <th className='text-center'>Serial</th>
+                                    <th className='text-center'>Photo</th>
+                                    <th className='text-center'>Name</th>
+                                    <th className='text-center'>Email</th>
+                                    <th className='text-center'>Price</th>
+                                    <th className='text-center'>Quantity</th>
+                                    <th className='text-center'>rating</th>
+                                    <th className='text-center'>Delete</th>
+
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    cartProduct.map((product, index) =>
+                                        <tr key={product._id} className='font-bold'>
+                                            <th className="text-center">{index + 1}</th>
+                                            <th className="text-center"><img className="w-24 mx-auto" src={product?.image} alt="" /></th>
+                                            <td className="text-center">{product.product_name}</td>
+                                            <td className="text-center">{product.email}</td>
+                                            <td className="text-center">${product.price}</td>
+                                            <td className="text-center">{product.quantity}</td>
+                                            <td className="text-center">{product.rating}</td>
+                                            <td className='text-center'><button onClick={() => handleDelete(product._id)} className='btn btn-error'>Delete</button></td>
+
+                                        </tr>)
+                                }
+
+                            </tbody>
+                        </table>
+                    </Fade>
+                </div>
+            </div>
         </div>
     );
 };
